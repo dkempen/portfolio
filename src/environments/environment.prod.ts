a856261e05f760e8c134a5a72221bc5ff3343191
Daan van Kempen <daan.vankempen2@gmail.com>
2020-07-31 22:59:44 +0200

    [Added] Firebase Analytics + lint fixes

--- src/environments/environment.prod.ts
+++ src/environments/environment.prod.ts
@@ -1,3 +1,13 @@
 export const environment = {
-  production: true
+  production: true,
+  firebaseConfig: {
+    apiKey: 'AIzaSyDrsHxU7BJhpoDYE0iuPgYfMqCm5a23Wb0',
+    authDomain: 'daanvankempen-f242d.firebaseapp.com',
+    databaseURL: 'https://daanvankempen-f242d.firebaseio.com',
+    projectId: 'daanvankempen-f242d',
+    storageBucket: 'daanvankempen-f242d.appspot.com',
+    messagingSenderId: '54855336027',
+    appId: '1:54855336027:web:1cb50527abc0df4428bf7e',
+    measurementId: 'G-5WWNK7Q60Z'
+  }
 };
