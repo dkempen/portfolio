0800827339013386f1ff7b86dd506626badd44b9
Daan van Kempen <daan.vankempen2@gmail.com>
2023-09-08 18:38:08 +0200

    Added automatic deployment with GitHub Action

--- src/environments/environment.prod.ts
+++ src/environments/environment.prod.ts
@@ -2,13 +2,13 @@ export const environment = {
   production: true,
   firebase: {
     projectId: 'daanvankempen-f242d',
-    appId: '***REMOVED***',
+    appId: '1:54855336027:web:1cb50527abc0df4428bf7e',
     databaseURL: 'https://daanvankempen-f242d.firebaseio.com',
     storageBucket: 'daanvankempen-f242d.appspot.com',
     locationId: 'europe-west',
-    apiKey: '***REMOVED***',
+    apiKey: 'AIzaSyDrsHxU7BJhpoDYE0iuPgYfMqCm5a23Wb0',
     authDomain: 'daanvankempen-f242d.firebaseapp.com',
-    messagingSenderId: '***REMOVED***',
-    measurementId: '***REMOVED***',
+    messagingSenderId: '54855336027',
+    measurementId: 'G-5WWNK7Q60Z',
   },
 };
