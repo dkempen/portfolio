326eb5199460a6a5a8ce048f875eff4aa1fd8599
dkempen <33512778+dkempen@users.noreply.github.com>
2024-05-04 14:40:06 +0200

    Removed the service worker

--- src/app/app.config.ts
+++ src/app/app.config.ts
@@ -60,7 +60,7 @@ export const appConfig: ApplicationConfig = {
     provideRouter(routes),
     importProvidersFrom([TranslateModule.forRoot(translateConfig)]),
     provideServiceWorker('ngsw-worker.js', {
-      enabled: environment.production,
+      enabled: false ?? environment.production,
       registrationStrategy: 'registerWhenStable:30000',
     }),
   ],
